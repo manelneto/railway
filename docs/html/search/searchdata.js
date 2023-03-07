9f80298b22ae78ba8048ff074ef0e2d6142825ba
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2023-03-07 22:09:21 +0000

    added doxyfile and generated empty documentation

---
+++ docs/html/search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "m",
+  1: "m",
+  2: "m"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "files",
+  2: "functions"
+};
+
+var indexSectionLabels =
+{
+  0: "Tudo",
+  1: "Ficheiros",
+  2: "Funções"
+};
+
