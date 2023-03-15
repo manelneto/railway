249c1600333f8a3924de4ead60d3edde87c6f908
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2023-03-15 11:46:21 +0000

    changed .gitignore and added management class

---
+++ Management.cpp
@@ -0,0 +1,5 @@
+//
+// Created by manue on 15/03/2023.
+//
+
+#include "Management.h"
---
+++ Management.h
@@ -0,0 +1,14 @@
+//
+// Created by manue on 15/03/2023.
+//
+
+#ifndef RAILWAY_MANAGEMENT_H
+#define RAILWAY_MANAGEMENT_H
+
+
+class Management {
+
+};
+
+
+#endif //RAILWAY_MANAGEMENT_H
