9048b9d676ab09c5acd6fc7f17e0655034cc566c
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2023-03-15 11:59:24 +0000

    added readStationsFile() (to be completed)

--- Management.cpp
+++ Management.cpp
@@ -3,3 +3,37 @@
 //
 
 #include "Management.h"
+#include "Station.h"
+#include <fstream>
+#include <sstream>
+#include <iostream>
+#include <vector> //???
+
+using namespace std;
+
+void Management::readStationsFile() {
+    ifstream in("../files/stations.csv");
+    if (!in.is_open()) {
+        cout << "Erro ao abrir o ficheiro stations.csv." << endl;
+        cout << "Verifique se o ficheiro se encontra dentro do diretório files." << endl;
+        return;
+    }
+    string fileLine;
+    getline(in, fileLine);
+    int number = 1;
+    while (getline(in, fileLine)) {
+        istringstream iss(fileLine);
+        string field;
+        vector<string> fields(6);
+        unsigned f = 0;
+        while (getline(iss, field, ','))
+            fields[f++] = field;
+        string name = fields[0];
+        string district = fields[1];
+        string municipality = fields[2];
+        string township = fields[3];
+        string line = fields[4];
+        Station station = Station(name, district, municipality, township, line);
+    }
+    cout << "Leitura de ficheiro stations.csv bem-sucedida." << endl;
+}
--- Management.h
+++ Management.h
@@ -7,7 +7,8 @@
 
 
 class Management {
-
+private:
+    void readStationsFile();
 };
 
 
