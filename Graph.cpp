ed17c8c2f745bdc23d50907a5a123e457f09de35
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2023-03-22 16:23:56 +0000

    removed documentation from Graph just to simplify (to be added later)

--- Graph.cpp
+++ Graph.cpp
@@ -12,9 +12,6 @@ std::vector<Vertex *> Graph::getVertexSet() const {
     return vertexSet;
 }
 
-/*
- * Auxiliary function to find a vertex with a given content.
- */
 Vertex * Graph::findVertex(const int &id) const {
     for (auto v : vertexSet)
         if (v->getId() == id)
@@ -22,19 +19,13 @@ Vertex * Graph::findVertex(const int &id) const {
     return nullptr;
 }
 
-/*
- * Finds the index of the vertex with a given content.
- */
 int Graph::findVertexIdx(const int &id) const {
     for (unsigned i = 0; i < vertexSet.size(); i++)
         if (vertexSet[i]->getId() == id)
             return i;
     return -1;
 }
-/*
- *  Adds a vertex with a given content or info (in) to a graph (this).
- *  Returns true if successful, and false if a vertex with that content already exists.
- */
+
 bool Graph::addVertex(const int &id) {
     if (findVertex(id) != nullptr)
         return false;
@@ -42,11 +33,6 @@ bool Graph::addVertex(const int &id) {
     return true;
 }
 
-/*
- * Adds an edge to a graph (this), given the contents of the source and
- * destination vertices and the edge weight (w).
- * Returns true if successful, and false if the source or destination vertex does not exist.
- */
 bool Graph::addEdge(const int &sourc, const int &dest, double w) {
     auto v1 = findVertex(sourc);
     auto v2 = findVertex(dest);
