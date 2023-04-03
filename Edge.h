592bba8ef522bb4d5df47d81be9a49cde472199c
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2023-04-03 21:00:39 +0100

    added documentation fields (i am filling them :))

--- Edge.h
+++ Edge.h
@@ -11,17 +11,80 @@ class Vertex;
 class Edge {
 public:
     enum Service {STANDARD, ALFA, OTHER};
+
+    /**
+     * <br>Complexidade Temporal: O()
+     * @param orig
+     * @param dest
+     * @param capacity
+     * @param service
+     */
     Edge(Vertex *orig, Vertex *dest, unsigned capacity, Service service);
+
+    /**
+     * <br>Complexidade Temporal: O()
+     * @return
+     */
     Vertex * getOrig() const;
+
+    /**
+     * <br>Complexidade Temporal: O()
+     * @return
+     */
     Vertex * getDest() const;
+
+    /**
+     * <br>Complexidade Temporal: O()
+     * @return
+     */
     unsigned getCapacity() const;
+
+    /**
+     * <br>Complexidade Temporal: O()
+     * @return
+     */
     Service getService() const;
+
+    /**
+     * <br>Complexidade Temporal: O()
+     * @return
+     */
     unsigned getCost() const;
+
+    /**
+     * <br>Complexidade Temporal: O()
+     * @return
+     */
     unsigned getFlow() const;
+
+    /**
+     * <br>Complexidade Temporal: O()
+     * @return
+     */
     bool isSelected() const;
+
+    /**
+     * <br>Complexidade Temporal: O()
+     * @return
+     */
     Edge * getReverse() const;
+
+    /**
+     * <br>Complexidade Temporal: O()
+     * @param flow
+     */
     void setFlow(unsigned flow);
+
+    /**
+     * <br>Complexidade Temporal: O()
+     * @param selected
+     */
     void setSelected(bool selected);
+
+    /**
+     * <br>Complexidade Temporal: O()
+     * @param reverse
+     */
     void setReverse(Edge * reverse);
 
 private:
