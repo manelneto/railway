72b4216175a4ad5cfccce56c6a640aa7ad9896a2
Manuel Neto <92862229+manelneto@users.noreply.github.com>
2023-04-03 13:32:24 +0100

    documented station

--- Station.h
+++ Station.h
@@ -9,20 +9,83 @@
 #include <string>
 
 class Station {
-public:
-    Station();
-    Station(const std::string &name);
-    Station(const std::string &name, const std::string &district, const std::string &municipality, const std::string &township, const std::string &line, int id);
-    const std::string &getSanitizedName() const;
-    const std::string &getName() const;
-    const std::string &getDistrict() const;
-    const std::string &getMunicipality() const;
-    const std::string &getTownship() const;
-    const std::string &getLine() const;
-    int getId() const;
-    bool operator==(const Station &rhs) const;
-    void print() const;
 
+public:
+    /**Construtor sem parâmetros. Constrói uma estação com todos os atributos vazios.
+     * <br>Complexidade Temporal: O(1)
+     */
+    Station();
+
+    /**Construtor com parâmetro. Sanitiza name e constrói uma estação com esse nome sanitizado e os restantes atributos vazios.
+     * <br>Complexidade Temporal: O(n), sendo n o comprimento de name
+     * @param name nome (a sanitizar)
+     */
+    Station(const std::string &name);
+
+    /**Construtor com parâmetros. Sanitiza name e constrói uma estação com esse nome sanitizado (para pesquisa) e por sanitizar (para imprimir), distrito district, concelho municipality, freguesia township, linha line e id id.
+     * <br>Complexidade Temporal: O(n), sendo n o comprimento de name
+     * @param name nome
+     * @param district distrito
+     * @param municipality concelho
+     * @param township freguesia
+     * @param line linha
+     * @param id id
+     */
+    Station(const std::string &name, const std::string &district, const std::string &municipality, const std::string &township, const std::string &line, int id);
+
+    /**Retorna o nome sanitizado da estação.
+     * <br>Complexidade Temporal: O(1)
+     * @return nome sanitizado da estação
+     */
+    const std::string &getSanitizedName() const;
+
+    /**Retorna o nome da estação.
+     * <br>Complexidade Temporal: O(1)
+     * @return nome da estação
+     */
+    const std::string &getName() const;
+
+    /**Retorna o distrito da estação.
+     * <br>Complexidade Temporal: O(1)
+     * @return distrito da estação
+     */
+    const std::string &getDistrict() const;
+
+    /**Retorna o concelho da estação.
+     * <br>Complexidade Temporal: O(1)
+     * @return concelho da estação
+     */
+    const std::string &getMunicipality() const;
+
+    /**Retorna a freguesia da estação.
+     * <br>Complexidade Temporal: O(1)
+     * @return freguesia da estação
+     */
+    const std::string &getTownship() const;
+
+    /**Retorna a linha da estação.
+     * <br>Complexidade Temporal: O(1)
+     * @return linha da estação
+     */
+    const std::string &getLine() const;
+
+    /**Retorna o id da estação.
+     * <br>Complexidade Temporal: O(1)
+     * @return id da estação
+     */
+    int getId() const;
+
+    /**Compara a estação e rhs quanto ao nome sanitizado.
+     * <br>Complexidade Temporal: O(n), sendo n o maior comprimento entre os nomes sanitizados
+     * @param rhs estação com a qual comparar
+     * @return true se a estação e rhs têm igual nome sanitizado, false caso contrário
+     */
+    bool operator==(const Station &rhs) const;
+
+    /**Imprime o nome, o distrito, o concelho, a freguesia e a linha da estação.
+     * <br>Complexidade Temporal: O(1)
+     */
+    void print() const;
 
 private:
     std::string sanitizedName;
@@ -36,6 +99,11 @@ private:
 };
 
 struct stationHash {
+    /**Função de dispersão para a tabela de dispersão de estações.
+     * <br>Complexidade Temporal: O(n), sendo n o comprimento do nome sanitizado
+     * @param station estação a mapear
+     * @return valor da hash para a estação a mapear
+     */
     int operator()(const Station &station) const {
         const std::string& str = station.getSanitizedName();
         int v = 0;
@@ -44,6 +112,12 @@ struct stationHash {
         return v;
     }
 
+    /**Função de igualdade para a tabela de dispersão de estações.
+     * <br>Complexidade Temporal: O(n), sendo n o maior comprimento entre os nomes sanitizados
+     * @param station1 estação 1
+     * @param station2 estação 2
+     * @return true se as estações são iguais, false caso contrário
+     */
     bool operator()(const Station &station1, const Station &station2) const {
         return station1 == station2;
     }
